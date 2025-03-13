e382b9aaebe8f463ce750aa0bbf0f2596f5373d9
Ajith S P <145632498+Giga-Roar@users.noreply.github.com>
2025-03-13 20:06:27 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "Hello, Giga Roar\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "CC labs is cool & fun \n";
+  return 0;
+}
